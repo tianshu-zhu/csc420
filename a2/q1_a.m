18421a0b0360f1fdf0b48669e8030dabf638b633
Tianshu <zhutians@teach.cs.toronto.edu>
2017-10-09 16:11:58 -0400

    working on q2 d

--- a2/q1_a.m
+++ a2/q1_a.m
@@ -1,19 +1,22 @@
 image = imread('building.jpg');
 % imagesc(image);
-I = double(rgb2gray(imread('building.jpg')));
+I = double(rgb2gray(image));
 [rows, cols] = size(I);
 
 % compute average Ix2, Iy2, Ixy
-[Ix, Iy] = imgradientxy(I);
-sigma = 5;
+[Ix, Iy] = imgradientxy(I, 'prewitt');
+sigma = 10;
 avg_Ix2 = imgaussfilt(Ix.^2, sigma);
 avg_Iy2 = imgaussfilt(Iy.^2, sigma);
 avg_Ixy = imgaussfilt(Ix.*Iy, sigma);
 
-% alpha is a constat (0.4 to 0.6)
-alpha = 0.05;
-% threshold = 20;
-R = (avg_Ix2.*avg_Iy2 - avg_Ixy.^2) - alpha.*(avg_Ix2 + avg_Iy2).^2;
+% alpha is a constat (0.04 to 0.06)
+alpha = 0.04;
+threshold = 20;
+R = (avg_Ix2.*avg_Iy2 - avg_Ixy.^2) - alpha.*((avg_Ix2 + avg_Iy2).^2);
+imagesc(R);
+max(R(:))
+min(R(:))
 
 % for row = 1:rows
 %     for col = 1:cols
---
+++ a2/q2.m
@@ -0,0 +1,45 @@
+% q2 a: extract sift frames and decriptors
+img1 = single(rgb2gray(imread('book.jpg')));
+img2 = single(rgb2gray(imread('findBook.jpg')));
+[f1, d1] = vl_sift(img1); % each col of f is [x; y; radius; orientation]
+[f2, d2] = vl_sift(img2); % each col of d is 128 d sift descriptor
+
+% q2 b: matching algorithm
+match = []; % each colomn of match is matched [i; j; dist]
+dists = pdist2(double(d1)', double(d2)');
+threshold = 0.8;
+for i = 1:size(d1, 2)
+    [min_dist1, j] = min(dists(i, :));
+    sorted_dists = sort(dists(i, :));
+    min_dist2 = sorted_dists(2);
+    rr = min_dist1/min_dist2; % reliability ratio
+    if  rr < threshold
+        match = [match [i; j; rr]];
+    end
+end
+match = sortrows(match', 3)';
+
+% q2 c: affine transformation
+k = 5;
+p1 = zeros(2*k, 6); % p2 = p1*a = T(p1)
+p2 = zeros(2*k, 1); % img1 transform to img2
+for i = 1:k
+    index1 = match(1, i);
+    index2 = match(2, i);
+    coor1 = f1(1:2, index1);
+    coor2 = f2(1:2, index2);
+    p1(2*i-1, :) = [coor1' 0 0 1 0];
+    p1(2*i, :) = [0 0 coor1' 0 1];
+    p2(2*i-1, 1) = coor2(1);
+    p2(2*i, 1) = coor2(2);
+end
+a = linsolve(p1' * p1, p1' * p2);
+
+% q2 d: visualize affine transformation
+A = [a(1) a(2) a(5); a(3) a(4) a(6)]; % reshape of a
+tl1 = [1; 1]; tr1 = [1; size(img1, 2)]; bl1 = [size(img1, 1); 1]; br1 = size(img1)'; % 4 corners of img1
+tl2 = A*[tl1; 1]; tr2 = A*[tr1; 1];
+bl2 = A*[bl1; 1]; br2 = A*[br1; 1];
+corners = [tl2 tr2 br2 bl2 tl2];
+figure;
+plot(corners(1, :), corners(2, :), 'b-');
