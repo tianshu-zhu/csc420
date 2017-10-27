6ec1343cb3474ad231ae2575e05b2b7ac86da534
Tianshu <zhutians@teach.cs.toronto.edu>
2017-10-27 18:46:05 -0400

    finished

--- a3/bonus.m
+++ a3/bonus.m
@@ -19,22 +19,59 @@ t_x = 2.5031875059141302e-02;
 t_z = -2.9342312935846411e-04;
 t_y = 6.6238747008330102e-04;
 
+%a
 load('rgbd.mat');
 K = [fx_d 0 px_d; 0 fy_d py_d; 0 0 1];
-wx = zeros(1, 640*480);
-wy = zeros(1, 640*480);
-w = zeros(1, 640*480);
-index = 1;
+
+xyz = zeros(480, 640, 3);
 for x = 1:640
     for y = 1:480
-        wx(index) = depth(y,x)*x;
-        wy(index) = depth(y,x)*y;
-        w(index) = depth(y,x);
-        index = index+1;
+        w = depth(y,x);
+        wx = w*x;
+        wy = w*y;
+        coor = linsolve(K, [wx; wy; w]);
+        xyz(y,x,:) = coor;
     end
 end
+x = reshape(xyz(:, :, 1), [1, 480*640]);
+y = reshape(xyz(:, :, 2), [1, 480*640]);
+z = reshape(xyz(:, :, 3), [1, 480*640]);
+figure; plot3(x, y, z);
 
-xyz = linsolve(K, [wx; wy; w]);
-x3 = xyz(1,:);
-y3 = xyz(2,:);
-z3 = xyz(3,:);
+%b
+[y1,x1] = find(labels==1);
+[y2,x2] = find(labels==2);
+[y3,x3] = find(labels==3);
+[y4,x4] = find(labels==4);
+
+xyz1 = zeros(size(x1, 1), 3);
+for i = 1:size(x1, 1)
+    xyz1(i,:) = xyz(y1(i), x1(i), :);
+end
+avgxyz1 = mean(xyz1, 1);
+d1 = norm(avgxyz1);
+h1 = (-1)*avgxyz1(2);
+
+xyz2 = zeros(size(x2, 1), 3);
+for i = 1:size(x2, 1)
+    xyz2(i,:) = xyz(y2(i), x2(i), :);
+end
+avgxyz2 = mean(xyz2, 1);
+d2 = norm(avgxyz2);
+h2 = (-1)*avgxyz2(2);
+
+xyz3 = zeros(size(x3, 1), 3);
+for i = 1:size(x3, 1)
+    xyz3(i,:) = xyz(y3(i), x3(i), :);
+end
+avgxyz3 = mean(xyz3, 1);
+d3 = norm(avgxyz3);
+h3 = (-1)*avgxyz3(2);
+
+xyz4 = zeros(size(x4, 1), 3);
+for i = 1:size(x4, 1)
+    xyz4(i,:) = xyz(y4(i), x4(i), :);
+end
+avgxyz4 = mean(xyz4, 1);
+d4 = norm(avgxyz4);
+h4 = (-1)*avgxyz4(2);
